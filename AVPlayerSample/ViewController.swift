f5abf9c873796981b952d449571d2bd7f11241c3
kanto <watanabe.kanto@gmail.com>
2019-05-03 00:23:33 +0900

    Initial Commit

---
+++ AVPlayerSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVPlayerSample
+//
+//  Created by 渡辺幹人 on 2019/05/03.
+//  Copyright © 2019 渡辺幹人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
