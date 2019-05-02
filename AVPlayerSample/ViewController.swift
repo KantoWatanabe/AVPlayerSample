de8e886031960b9b061392d67b380eaf64d24b75
kanto <watanabe.kanto@gmail.com>
2019-05-03 03:11:15 +0900

    AVPlayerViewControllerによる動画再生機能追加

--- AVPlayerSample/ViewController.swift
+++ AVPlayerSample/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    // MARK: View LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // MARK: IBActions
+    
+    @IBAction func avPlayerViewControllerBtnTapped(_ sender: Any) {
+        playByAVPlayerViewController()
+    }
+    
+    // MARK: private method
+    
+    // https://developer.apple.com/documentation/avfoundation/media_assets_playback_and_editing/creating_a_basic_video_player_ios_and_tvos
+    private func playByAVPlayerViewController() {
+        
+        // https://stackoverflow.com/questions/41112283/avplayer-not-playing-m3u8-from-local-file
+        // ローカルのm3u8は再生できない、mp4であれば可能
+        // AndroidのExoPlayerはできてしまうが。。
+        //guard let m3u8Path = Bundle.main.path(forResource: "sample", ofType: "m3u8") else {
+        //    return
+        //}
+        
+        //let avPlayer = AVPlayer(url: URL(fileURLWithPath: m3u8Path))
+        
+        guard let hlsUrl = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8") else {
+            return
+        }
+        
+        let avPlayer = AVPlayer(url: hlsUrl)
+        
+        let avPlayerViewController = AVPlayerViewController()
+        avPlayerViewController.player = avPlayer
+        
+        self.present(avPlayerViewController, animated: true, completion: {
+            avPlayer.play()
+        })
+    }
 }
 
