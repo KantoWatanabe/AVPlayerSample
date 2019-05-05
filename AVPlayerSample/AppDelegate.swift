653f58232feb180d98eabb723e4e7f5fceda7b70
kanto <watanabe.kanto@gmail.com>
2019-05-05 13:10:55 +0900

    BG再生を可能にした

--- AVPlayerSample/AppDelegate.swift
+++ AVPlayerSample/AppDelegate.swift
@@ -17,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // BG再生を可能にする
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(.playback, mode: .moviePlayback)
+        }
+        catch {
+            print("Setting category to AVAudioSessionCategoryPlayback failed.")
+        }
         return true
     }
 
